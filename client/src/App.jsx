38545f13bbd5d3d2012a6462c37a79a51b9f552e
kobrak1 <burakkarhan657@gmail.com>
2024-07-02 11:59:04 +0300

    chore: the directory of Header component has been rewritten

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom"
 import HomePage from "./pages/Homepage"
-import Header from "./components/Header"
+import Header from "./components/Header/Header"
 
 const App = () => {
   return (
