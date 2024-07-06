2b88b8028d1e80f3f7077b2f6e01bac2874bd905
kobrak1 <aburakkarhan@gmail.com>
2024-07-06 20:13:19 +0300

    feature: Blogs component adjusted to fetch the data from the db with useEffect hook

--- client/src/components/Blogs/Blogs.jsx
+++ client/src/components/Blogs/Blogs.jsx
@@ -1,7 +1,19 @@
+import { useEffect, useState } from "react"
+import blogService from '../../services/blogService'
+
 const Blogs = () => {
+    const [blogs, setBlogs] = useState([])
+
+    useEffect(() => {
+        blogService.getAll()
+          .then(data => setBlogs(data))
+    }, [blogs])
+
     return(
-        <div>
-            All Blogs
+        <div className="flex">
+            {blogs.map(item =>
+                <li key={item.id} className="flex container border border-slate-200 rounded-md max-w-custom p-1 my-2 mx-auto text-slate-600"> {item.content} </li>
+            )}
         </div>
     )
 }
