24c86696b019400b72cbc9202fe4d160639f9fef
kobrak1 <burakkarhan657@gmail.com>
2024-09-07 23:57:25 +0300

    update: the jst decoder library has been removed from App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,8 @@
 import { Route, Routes } from "react-router-dom"
 import HomePage from "./pages/Homepage"
 import Header from "./components/Header/Header"
-import { decodeToken } from '@leteu/jwt-decoder'
 
 const App = () => {
-  const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNhbnNhckBnbWFpbC5jb20iLCJpZCI6IjY2N2YxYTQwYjM0OGYyZDkyNGQ0NWIxNiIsImlhdCI6MTcyMDcyMDI5NSwiZXhwIjoxNzIwNzIzODk1fQ.AQes3OV8S_ll_IHanPZ1mfxQ6dzLaDHfD1br0QeZob8'
-  console.log(decodeToken(token))
   return (
     <div>
       <Header />
